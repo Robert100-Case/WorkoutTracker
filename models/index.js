153e86fa958c790548c9878c86eb34cb0f0c4ba1
Bob <77537418+Robert100-Case@users.noreply.github.com>
2021-06-28 13:13:30 -0400

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Workout = require('./workout');
+//const Project = require('./Project');
+
+/*User.hasMany(Project, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Project.belongsTo(User, {
+  foreignKey: 'user_id'
+});*/
+
+module.exports = { Workout };
